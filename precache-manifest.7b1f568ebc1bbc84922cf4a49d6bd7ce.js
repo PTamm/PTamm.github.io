25dacebc82c9018d293fe88d030a2da9f07a626d
PTamm <tamm@ualberta.ca>
2019-02-16 02:21:11 +0000

    new attempt

---
+++ precache-manifest.7b1f568ebc1bbc84922cf4a49d6bd7ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0178d7651ed595ff00c",
+    "url": "/static/js/main.d0178d76.chunk.js"
+  },
+  {
+    "revision": "190f2a20ea61e6b8fc5f",
+    "url": "/static/js/1.190f2a20.chunk.js"
+  },
+  {
+    "revision": "d0178d7651ed595ff00c",
+    "url": "/static/css/main.d6e1afe2.chunk.css"
+  },
+  {
+    "revision": "59cab55c795006ebcf262e10e27998eb",
+    "url": "/index.html"
+  }
+];
